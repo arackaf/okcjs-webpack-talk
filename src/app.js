02e57de174b66f8e813a9ef886e43da0433b3cbb
Adam Rackis <adamrackis@gmail.com>
2017-07-13 22:07:04 -0500

    code splitting

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
-import Contacts from './contacts/contacts';
-import Tasks from './tasks/tasks';
+//import Contacts from ;
+//import Tasks from './tasks/tasks';
 
 let contactsBtn = document.getElementById('contacts-btn'),
     tasksBtn = document.getElementById('tasks-btn');
 
-contactsBtn.addEventListener('click', () => Contacts.doContactsThing());
+contactsBtn.addEventListener('click', () => {
+    import('./contacts/contacts').then(({default: Contacts}) => Contacts.doContactsThing())    
+});
 
-tasksBtn.addEventListener('click', () => Tasks.doTasksThing());
+tasksBtn.addEventListener('click', () => {
+    import('./tasks/tasks').then(({default: Tasks}) => Tasks.doTasksThing())    
+});
