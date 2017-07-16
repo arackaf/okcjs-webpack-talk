528bea091caf8c06282f17288074e67f1b4df3de
Adam Rackis <adamrackis@gmail.com>
2017-07-16 01:17:31 -0500

    update to new webpack loader format

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,14 @@ module.exports = {
         publicPath: 'dist/'
     },
     module: {
-        loaders: [{
+        rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: [["es2015", { "modules": false }], 'stage-2']
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [["es2015", { "modules": false }], 'stage-2']
+                }
             }
         }]
     },
